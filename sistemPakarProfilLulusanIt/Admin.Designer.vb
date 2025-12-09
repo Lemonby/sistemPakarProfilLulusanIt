4641e5e9f2cdc288ec68c5a1052f8efdc8381960
pnugraha18@gmail.com <pnugraha18@gmail.com>
2025-12-09 17:43:49 +0700

    add layout admin

--- sistemPakarProfilLulusanIt/Admin.Designer.vb
+++ sistemPakarProfilLulusanIt/Admin.Designer.vb
@@ -22,26 +22,169 @@ Partial Class Admin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Admin))
         Panel1 = New Panel()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        Button2 = New Button()
+        Button1 = New Button()
+        Button3 = New Button()
+        DataGridView1 = New DataGridView()
+        DataGridView2 = New DataGridView()
+        Button4 = New Button()
+        Panel1.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
+        Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), Image)
+        Panel1.Controls.Add(Button4)
+        Panel1.Controls.Add(DataGridView2)
+        Panel1.Controls.Add(DataGridView1)
+        Panel1.Controls.Add(Button3)
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(TextBox1)
         Panel1.Location = New Point(1, 0)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(804, 453)
+        Panel1.Size = New Size(804, 524)
         Panel1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.White
+        Label2.Image = CType(resources.GetObject("Label2.Image"), Image)
+        Label2.Location = New Point(31, 51)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(80, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Username"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(31, 83)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(299, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Image = CType(resources.GetObject("Label1.Image"), Image)
+        Label1.Location = New Point(31, 129)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(76, 20)
+        Label1.TabIndex = 6
+        Label1.Text = "Password"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(31, 161)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(299, 27)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.DodgerBlue
+        Button2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = Color.White
+        Button2.Location = New Point(31, 212)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 37)
+        Button2.TabIndex = 21
+        Button2.Text = "Simpan"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LimeGreen
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(150, 212)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 37)
+        Button1.TabIndex = 22
+        Button1.Text = "Update"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Red
+        Button3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = Color.White
+        Button3.Location = New Point(266, 212)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 37)
+        Button3.TabIndex = 23
+        Button3.Text = "Delete"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(11, 273)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(776, 238)
+        DataGridView1.TabIndex = 24
+        ' 
+        ' DataGridView2
+        ' 
+        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView2.Location = New Point(390, 12)
+        DataGridView2.Name = "DataGridView2"
+        DataGridView2.RowHeadersWidth = 51
+        DataGridView2.Size = New Size(393, 187)
+        DataGridView2.TabIndex = 25
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.Red
+        Button4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.ForeColor = Color.White
+        Button4.Location = New Point(693, 237)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 30)
+        Button4.TabIndex = 26
+        Button4.Text = "Print"
+        Button4.UseVisualStyleBackColor = False
         ' 
         ' Admin
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(800, 523)
         Controls.Add(Panel1)
         Name = "Admin"
         Text = "Admin"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button4 As Button
 End Class
